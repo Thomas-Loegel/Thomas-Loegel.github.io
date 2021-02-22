f892c9a4c06eb5dea4f35188b5784e9e84a98acf
Thomas-Loegel <t.loegel@gmail.com>
2021-02-22 22:58:31 +0100

    issue #1 & #2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis cupiditate sequi harum commodi fugiat voluptas eius facilis sit quis! Voluptatem tempora libero ducimus deserunt quas atque repudiandae. Sequi, assumenda. Itaque?</p>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
